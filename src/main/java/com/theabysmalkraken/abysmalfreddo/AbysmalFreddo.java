f5e4b9bb57260686ef0a884e87f27e4c62a8e3ab
Jamie Canning <theabysmalkraken>
2020-12-05 22:09:15 +0000

    Fix client not loading

--- src/main/java/com/theabysmalkraken/abysmalfreddo/AbysmalFreddo.java
+++ src/main/java/com/theabysmalkraken/abysmalfreddo/AbysmalFreddo.java
@@ -1,32 +1,48 @@
 package com.theabysmalkraken.abysmalfreddo;
 
+import org.apache.logging.log4j.LogManager;
+
 import com.theabysmalkraken.abysmalfreddo.item.FreddoItem;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.*;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.fml.RegistryObject;
+import org.apache.logging.log4j.Logger;
 
 import net.minecraft.item.Item;
 
-@Mod(value = AbysmalFreddo.MOD_ID)
+@Mod(AbysmalFreddo.MOD_ID)
 public class AbysmalFreddo {
 	public static AbysmalFreddo instance;
 	public static final String MOD_ID = "abysmalfreddo";
 	
+	public static final Logger LOGGER = LogManager.getLogger();
+	
 	private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
-	public static final RegistryObject<Item> FREDDO_ITEM = ITEMS.register("freddo", () -> new FreddoItem());
+	public static final RegistryObject<Item> FREDDO_ITEM = ITEMS.register("freddo", FreddoItem::new);
 	
 	public AbysmalFreddo() {
 		instance = this;
 		final IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
 		modBus.addListener(this::commonSetup);
+		modBus.addListener(this::clientSetup);
+		
+		ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+		
+		MinecraftForge.EVENT_BUS.register(this);
 	}
 	
 	private void commonSetup(FMLCommonSetupEvent event) {
+		
+	}
+	
+	private void clientSetup(FMLClientSetupEvent event) {
 		
 	}
 }
--- src/main/java/com/theabysmalkraken/abysmalfreddo/item/FreddoItem.java
+++ src/main/java/com/theabysmalkraken/abysmalfreddo/item/FreddoItem.java
@@ -6,7 +6,6 @@ import net.minecraft.item.Food;
 
 
 public class FreddoItem extends Item{
-	@ObjectHolder("abysmalfreddo:freddo")
 	public static final Food freddo = (new Food.Builder()).hunger(2).saturation(0.5F).setAlwaysEdible().fastToEat().build();
 
 	public FreddoItem() {
