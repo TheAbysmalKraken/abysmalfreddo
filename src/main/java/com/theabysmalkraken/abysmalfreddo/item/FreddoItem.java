3f68bc7ed1eff45acb9508fba5162e224d280fe6
Jamie Canning <theabysmalkraken>
2020-12-05 22:22:55 +0000

    Finish Freddo item

--- src/main/java/com/theabysmalkraken/abysmalfreddo/item/FreddoItem.java
+++ src/main/java/com/theabysmalkraken/abysmalfreddo/item/FreddoItem.java
@@ -1,6 +1,7 @@
 package com.theabysmalkraken.abysmalfreddo.item;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraftforge.registries.ObjectHolder;
 import net.minecraft.item.Food;
 
@@ -9,7 +10,7 @@ public class FreddoItem extends Item{
 	public static final Food freddo = (new Food.Builder()).hunger(2).saturation(0.5F).setAlwaysEdible().fastToEat().build();
 
 	public FreddoItem() {
-		super(new Item.Properties().food(freddo));
+		super(new Item.Properties().group(ItemGroup.FOOD).food(freddo));
 	}
 
 }
